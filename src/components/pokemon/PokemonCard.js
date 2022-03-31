44b90dcbdc560149d7d32f130d44ac3c6d88c332
cameronsassman <cjs.sassy01@gmail.com>
2022-03-31 16:22:52 +0200

    commit

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom"; 
 import { useState } from 'react'
 import Modal from 'react-modal'
 
@@ -7,6 +6,11 @@ import classes from './PokemonCard.module.css'
 import '../ui/Modal.css'
 
 const PokeCard = ({ pokemon }) => {
+
+    // const style = {
+    //     background: `linear-gradient(100deg, ${PokemonType[pokemon.types[0].type.name]} 25%, white 15%)`
+    // }
+
     const [modalIsOpen, setIsOpen] = useState(false)
     const [toggleState, setToggleState] = useState(1);
 
@@ -32,7 +36,8 @@ const PokeCard = ({ pokemon }) => {
             >
                 <div 
                     className={classes.content} 
-                    style={{ background: `linear-gradient(100deg, ${PokemonType[pokemon.types[0].type.name]} 25%, white 15%)`}}
+                    style={{ background: PokemonType[pokemon.types[0].type.name]}}
+                    // style={{ background: `linear-gradient(100deg, ${PokemonType[pokemon.types[0].type.name]} 25%, white 15%)`,}}
                 >
                 <div className={classes.pokeid}>
                 <span>#{pokemon.id}</span>
@@ -43,84 +48,86 @@ const PokeCard = ({ pokemon }) => {
                         <span className={classes.namedescspan}>{pokemon.name}</span>
                     </div>
                 </div>
-                <div className="bloc-tabs">
-                    <button
-                        className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-                        onClick={() => toggleTab(1)}
-                    >
-                        Description
-                    </button>
-                    <button
-                        className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-                        onClick={() => toggleTab(2)}
-                    >
-                        Stats
-                    </button>
-                    <button
-                        className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
-                        onClick={() => toggleTab(3)}
-                    >
-                        Moves
-                    </button>
-                </div>
-                <div className="content-tabs">
-                <div className={toggleState === 1 ? "content  active-content" : "content"}>
-                    <div className="pokemon--stats">
-                        <div className={classes.pokestats}>
-                            <div>
-                                Base Exp:
-                                <span className={classes.descspan}>{pokemon.base_experience}</span>
-                            </div>
-                            <div>
-                                Weight: 
-                                <span className={classes.descspan}>{pokemon.weight}</span>
-                            </div>
-                            <div>
-                                Height:
-                                <span className={classes.descspan}>{pokemon.height}</span>
-                            </div>
-                            <div className={classes.ability}>
-                                Ability:
-                                <span className={classes.descspan}>{pokemon.abilities[0].ability.name}</span>
+                <div className={classes.pokeinfo}>
+                    <div className="bloc-tabs">
+                        <button
+                            className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+                            onClick={() => toggleTab(1)}
+                        >
+                            Description
+                        </button>
+                        <button
+                            className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+                            onClick={() => toggleTab(2)}
+                        >
+                            Stats
+                        </button>
+                        <button
+                            className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+                            onClick={() => toggleTab(3)}
+                        >
+                            Moves
+                        </button>
+                    </div>
+                    <div className="content-tabs">
+                        <div className={toggleState === 1 ? "content  active-content" : "content"}>
+                            <div className="pokemon--stats">
+                                <div className={classes.pokestats}>
+                                    <div>
+                                        Base Exp:
+                                        <span className={classes.descspan}>{pokemon.base_experience}</span>
+                                    </div>
+                                    <div>
+                                        Weight: 
+                                        <span className={classes.descspan}>{pokemon.weight}</span>
+                                    </div>
+                                    <div>
+                                        Height:
+                                        <span className={classes.descspan}>{pokemon.height}</span>
+                                    </div>
+                                    <div className={classes.ability}>
+                                        Ability:
+                                        <span className={classes.descspan}>{pokemon.abilities[0].ability.name}</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className={toggleState === 2 ? "content  active-content" : "content"}>
-                    <div className="pokemon--stats">
-                        {
-                            pokemon.stats.map(stat => {
-                                return (
-                                    <div className="pokemon-stat-item">
-                                        <div>{stat.stat.name}</div>
-                                        <div>{stat.base_stat}</div>
-                                        <div className="progress-bar" style={{ background: PokemonType[pokemon.types[0].type.name]}}>
-                                            <span
-                                                style={{
-                                                    width: `${100 - stat.base_stat || 0}%`,
-                                                }}
-                                            />
-                                        </div>
-                                    </div>
-                            );
-                            })
-                        }
-                    </div>
-                </div>
-                    <div className={toggleState === 3 ? "content  active-content" : "content"}>
-                    <div className={classes.pokestats}>
-                    <ul className="scroll">
-                        {
-                            pokemon.moves.map(move => {
-                                return (
-                                <span className={classes.type}>
-                                    {move.move.name}
-                                </span>
-                                )
-                            })
-                        }
-                    </ul>
-                    </div>
+                        <div className={toggleState === 2 ? "content  active-content" : "content"}>
+                            <div className="pokemon--stats">
+                                {
+                                    pokemon.stats.map(stat => {
+                                        return (
+                                            <div className="pokemon-stat-item">
+                                                <div>{stat.stat.name}</div>
+                                                <div>{stat.base_stat}</div>
+                                                <div className="progress-bar" style={{ background: PokemonType[pokemon.types[0].type.name]}}>
+                                                    <span
+                                                        style={{
+                                                            width: `${100 - stat.base_stat || 0}%`,
+                                                        }}
+                                                    />
+                                                </div>
+                                            </div>
+                                    );
+                                    })
+                                }
+                            </div>
+                        </div>
+                        <div className={toggleState === 3 ? "content  active-content" : "content"}>
+                            <div className={classes.pokestats}>
+                                <ul className="scroll">
+                                    {
+                                        pokemon.moves.map(move => {
+                                            return (
+                                            <span className={classes.type}>
+                                                {move.move.name}
+                                            </span>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 </div>
@@ -130,35 +137,3 @@ const PokeCard = ({ pokemon }) => {
 }
 
 export default PokeCard;
-
-
-{/* <div className={classes.pokestats}>
-<div>
-    Type:
-    {
-        pokemon.types.map(type => {
-            return (
-            <span className={classes.type}>
-                {type.type.name}
-            </span>
-            )
-        })
-    }
-</div>
-    <div>
-        Base Exp:
-        <span className={classes.descspan}>{pokemon.base_experience}</span>
-    </div>
-    <div>
-        Weight: 
-        <span className={classes.descspan}>{pokemon.weight}</span>
-    </div>
-    <div>
-        Height:
-        <span className={classes.descspan}>{pokemon.height}</span>
-    </div>
-    <div className={classes.ability}>
-        Ability
-        <span className={classes.descspan}>{pokemon.abilities[0].ability.name}</span>
-    </div>
-</div> */}
--- src/pages/PokemonDetails.js
+++ src/pages/PokemonDetails.js
@@ -1,105 +1,105 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import PokeDetails from "../components/pokemon/PokeDetails";
-import LoadPokeball from "../components/ui/Loader";
-import "../index.css"
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import PokeDetails from "../components/pokemon/PokeDetails";
+// import LoadPokeball from "../components/ui/Loader";
+// import "../index.css"
 
-const PokemonDetail = () => {
-    const [isPokemon, setIsPokemon] = useState([])
-    const [loadPokemon, setLoadPokemon] = useState('https://pokeapi.co/api/v2/pokemon/')
-    const [isLoading, setIsLoading] = useState(true)
-    const [toggleState, setToggleState] = useState(1);
+// const PokemonDetail = () => {
+//     const [isPokemon, setIsPokemon] = useState([])
+//     const [loadPokemon, setLoadPokemon] = useState('https://pokeapi.co/api/v2/pokemon/')
+//     const [isLoading, setIsLoading] = useState(true)
+//     const [toggleState, setToggleState] = useState(1);
 
-    const toggleTab = (index) => {
-      setToggleState(index);
-    };
+//     const toggleTab = (index) => {
+//       setToggleState(index);
+//     };
 
-    const { pokeId } = useParams();
+//     const { pokeId } = useParams();
 
-    const getPokemon = async () => {
-        const res = await fetch(loadPokemon)
-        const data = await res.json()
-        setIsLoading(false)
-        console.log(data)
-        setLoadPokemon(data)
+//     const getPokemon = async () => {
+//         const res = await fetch(loadPokemon)
+//         const data = await res.json()
+//         setIsLoading(false)
+//         console.log(data)
+//         setLoadPokemon(data)
 
-        function createPokemonObject(results)  {
-             fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
-             .then(res => res.json())
-             .then (data => setIsPokemon(currentList => [...currentList, data]))
-          }
-          createPokemonObject(data.results)
-    }
+//         function createPokemonObject(results)  {
+//              fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`)
+//              .then(res => res.json())
+//              .then (data => setIsPokemon(currentList => [...currentList, data]))
+//           }
+//           createPokemonObject(data.results)
+//     }
 
-    useEffect(() => {
-        getPokemon()
-    }, [])
+//     useEffect(() => {
+//         getPokemon()
+//     }, [])
     
-    return (
-        <section>
-            {isLoading ? <LoadPokeball /> : (
-            <div className="container">
-                <div className="bloc-tabs">
-                    <button
-                        className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-                        onClick={() => toggleTab(1)}
-                    >
-                        Tab 1
-                    </button>
-                    <button
-                        className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-                        onClick={() => toggleTab(2)}
-                    >
-                        Tab 2
-                    </button>
-                    <button
-                        className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
-                        onClick={() => toggleTab(3)}
-                    >
-                        Tab 3
-                    </button>
-                </div>
-                    <div className="content-tabs">
-                        <div
-                            className={toggleState === 1 ? "content  active-content" : "content"}
-                        >
-                            {isPokemon.map((pokemon, poke) => {
-                            return (
-                                <PokeDetails
-                                    key={poke}
-                                    pokemon={pokemon}
-                                />
-                            )
-                        })}
-                        </div>
-                        <div
-                            className={toggleState === 2 ? "content  active-content" : "content"}
-                        >
-                            {isPokemon.map((pokemon, poke) => {
-                            return (
-                                <PokeDetails
-                                    key={poke}
-                                    pokemon={pokemon}
-                                />
-                            )
-                        })}
-                        </div>
-                        <div className={toggleState === 3 ? "content  active-content" : "content"}>
-                            <h1>Test 3</h1>
-                        </div>
-                    </div>
-                    {/* {isPokemon.map((pokemon, poke) => {
-                        return (
-                            <PokeDetails
-                                key={poke}
-                                pokemon={pokemon}
-                            />
-                        )
-                    })} */}
-                </div>
-            )}
-        </section>
-    )
-}
+//     return (
+//         <section>
+//             {isLoading ? <LoadPokeball /> : (
+//             <div className="container">
+//                 <div className="bloc-tabs">
+//                     <button
+//                         className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+//                         onClick={() => toggleTab(1)}
+//                     >
+//                         Tab 1
+//                     </button>
+//                     <button
+//                         className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+//                         onClick={() => toggleTab(2)}
+//                     >
+//                         Tab 2
+//                     </button>
+//                     <button
+//                         className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+//                         onClick={() => toggleTab(3)}
+//                     >
+//                         Tab 3
+//                     </button>
+//                 </div>
+//                     <div className="content-tabs">
+//                         <div
+//                             className={toggleState === 1 ? "content  active-content" : "content"}
+//                         >
+//                             {isPokemon.map((pokemon, poke) => {
+//                             return (
+//                                 <PokeDetails
+//                                     key={poke}
+//                                     pokemon={pokemon}
+//                                 />
+//                             )
+//                         })}
+//                         </div>
+//                         <div
+//                             className={toggleState === 2 ? "content  active-content" : "content"}
+//                         >
+//                             {isPokemon.map((pokemon, poke) => {
+//                             return (
+//                                 <PokeDetails
+//                                     key={poke}
+//                                     pokemon={pokemon}
+//                                 />
+//                             )
+//                         })}
+//                         </div>
+//                         <div className={toggleState === 3 ? "content  active-content" : "content"}>
+//                             <h1>Test 3</h1>
+//                         </div>
+//                     </div>
+//                     {/* {isPokemon.map((pokemon, poke) => {
+//                         return (
+//                             <PokeDetails
+//                                 key={poke}
+//                                 pokemon={pokemon}
+//                             />
+//                         )
+//                     })} */}
+//                 </div>
+//             )}
+//         </section>
+//     )
+// }
 
-export default PokemonDetail
+// export default PokemonDetail
